c6e5ef10345403e7069a770ce577776c2fa4fd56
soonhyeok <60051832+soonhyeok@users.noreply.github.com>
2020-02-07 18:39:29 +0900

    Create Chap03_06_Trapping_Rain.py

---
+++ Chap03_06_Trapping_Rain.py
@@ -0,0 +1,17 @@
+@ -0,0 +1,16 @@
+def trapping_rain(buildings):
+    sum_rain = 0  # 총 빗물량
+    for i in range(1, len(buildings) - 1):
+        left_building = max(buildings[:i])
+        right_building = max(buildings[i:])
+
+        rain_height = min(left_building, right_building)
+
+        sum_rain = sum_rain + max(0, rain_height - buildings[i])
+
+    return sum_rain
+
+
+# 테스트
+print(trapping_rain([3, 0, 0, 2, 0, 4]))
+print(trapping_rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
