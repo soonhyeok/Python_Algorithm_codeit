47a182ca01307803916afe7168b63ad35948d030
soonhyeok <saenec8598@gmail.com>
2020-02-17 18:08:15 +0900

    Update 12_max_profit.py

--- Chap06/12_max_profit.py
+++ Chap06/12_max_profit.py
@@ -30,3 +30,22 @@ def max_profit(price_list, count):
 print(max_profit([0, 100, 400, 800, 900, 1000], 5))
 print(max_profit([0, 100, 400, 800, 900, 1000], 10))
 print(max_profit([0, 100, 400, 800, 900, 1000, 1400, 1600, 2100, 2200], 9))
+
+
+def sublist_max(profits):
+
+    max_value = 0
+    temp_value = 0
+
+    for i in profits:
+        temp_value += i
+        if max_value < temp_value:
+            max_value = temp_value
+
+    return max_value
+
+
+# 테스트
+print(sublist_max([4, 3, 8, -2, -5, -3, -5, -3]))
+print(sublist_max([2, 3, 1, -1, -2, 5, -1, -1]))
+print(sublist_max([7, -3, 14, -8, -5, 6, 8, -5, -4, 10, -1, 8]))
